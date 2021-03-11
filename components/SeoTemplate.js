22fffe22b7b5edcb2bdd121e5dd815f170584afc
fervillalbag <21fervillalbag@gmail.com>
2021-03-11 18:37:34 -0300

    Addying meta description

--- components/SeoTemplate.js
+++ components/SeoTemplate.js
@@ -5,6 +5,7 @@ export default function SeoTemplate() {
   return (
     <Head>
       <title>Landing Main | Atomic Design</title>
+      <meta name="description" content="Bring everyone together to build better products." />
       <link href="https://fonts.googleapis.com/css2?family=Be+Vietnam:wght@400;500;700&display=swap" rel="stylesheet" />
     </Head>
   )
